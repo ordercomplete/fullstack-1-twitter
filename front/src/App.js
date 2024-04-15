f37bc052a0dd5288266379b8c586871f1f50c88d
ordercomplete <ordercomplete777@gmail.com>
2024-04-16 00:31:35 +0300

    Hook useRef useMemo useCallback

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,26 @@
+import { useRef, useEffect, useState, useMemo, useCallback } from "react";
 import Page from "./component/page";
-import PostList from "./container/post-list";
-function App() {
+
+export default function TodoList() {
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Придбати продукти" },
+    { id: 2, text: "Прибрати вдома" },
+    { id: 3, text: "Зробити домашнє завдання" },
+  ]);
+  // Кешуємо функцію видалення завдання за допомогою `useCallback`
+  const handleDeleteTodo = useCallback((id) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  }, []);
   return (
-    <Page>
-      <PostList />
-    </Page>
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          <span>{todo.text}</span>
+          <button onClick={() => handleDeleteTodo(todo.id)}> Видалити</button>
+        </li>
+      ))}
+    </ul>
   );
 }
-export default App;
+
+// export default App;
